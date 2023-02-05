2f59542a5a7f4f055161e846e0aef1f72b8bb5ef
tavarod82jsociety <106413863+tavarod82jsociety@users.noreply.github.com>
2023-02-05 13:21:04 -0500

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,10 @@
+contador = 0
+while contador <= 60:
+    cuadrado = contador*contador
+    print(f"El cuadrado de {contador} es {contador}▓▓")
+    contador += 1
+    
+contador = 1
+while contador <= 60:
+    print(f"El cuadrado de {contador} es {contador**2}☺")
+    contador += 1
